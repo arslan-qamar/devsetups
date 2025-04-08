207becff4c23469db29f5eb7a13e4c532143a9cc
arslan-qamar <ravian720@gmail.com>
2025-04-09 06:55:34 +1000

    Update InteractiveBrokers.sh

--- InteractiveBrokers.sh
+++ InteractiveBrokers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# Update package list
+# Install dependencies
 sudo apt update 
+sudo apt install wget gpg
 
 # Install Git 
 sudo apt install git
@@ -16,8 +17,17 @@ sudo apt install curl
 # Install Devbox
 sudo curl -fsSL https://get.jetpack.io/devbox | bash -s -- --force
 
-# Install VsCode
-sudo snap install code
+# Import the Microsoft GPG key
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+# Add the VS Code repo
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 # Login Github
 gh auth login
