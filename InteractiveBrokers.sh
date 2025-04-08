8b0dcd3964492cde3cd63790336db5e4567434c4
arslan-qamar <ravian720@gmail.com>
2025-04-09 07:02:09 +1000

    Update InteractiveBrokers.sh

--- InteractiveBrokers.sh
+++ InteractiveBrokers.sh
@@ -15,14 +15,14 @@ sudo apt install gh
 sudo apt install curl
 
 # Install Devbox
-sudo curl -fsSL https://get.jetpack.io/devbox | bash -s -- --force
+curl -fsSL https://get.jetpack.io/devbox | bash -s -- --force
 
 # Import the Microsoft GPG key
 sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+sudo echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+sudo rm -f packages.microsoft.gpg
 
 # Add the VS Code repo
 sudo apt install apt-transport-https
@@ -38,5 +38,3 @@ gh repo clone https://github.com/arslan-qamar/interactivebrokers2.git
 
 cd interactivebrokers2
 devbox init || true   # don't fail if it already has devbox.json
-
-
