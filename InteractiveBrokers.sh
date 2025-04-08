5221bd4a89a6800df1688c5531e0cfa8b54cb816
arslan-qamar <ravian720@gmail.com>
2025-04-09 06:39:16 +1000

    Update InteractiveBrokers.sh

--- InteractiveBrokers.sh
+++ InteractiveBrokers.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 # Update package list
-sudo apt update
+sudo apt update -y
 
 # Install Git 
 sudo apt install -y git
 
 # Install Github cli (for cloning repos)
-sudo apt install gh
+sudo apt install -y gh
 
 # Install curl (required to fetch Devbox install script)
 sudo apt install -y curl
@@ -17,11 +17,14 @@ sudo apt install -y curl
 sudo curl -fsSL https://get.jetpack.io/devbox | bash
 
 # Install VsCode
-sudo snap install code
+sudo snap install -y code
+
+# Login Github
+gh auth login
 
 # Clone your target repository
 sudo rm -rf interactivebrokers2
-git clone https://github.com/arslan-qamar/interactivebrokers2.git
+gh repo clone https://github.com/arslan-qamar/interactivebrokers2.git
 
 cd interactivebrokers2
 devbox init || true   # don't fail if it already has devbox.json
