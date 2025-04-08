6e6b8cc5a5db426737f6942ab43b65a21692a82f
arslan-qamar <ravian720@gmail.com>
2025-04-09 07:07:08 +1000

    Update InteractiveBrokers.sh

--- InteractiveBrokers.sh
+++ InteractiveBrokers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+cd ~
+
 # Install dependencies
 sudo apt update 
 sudo apt install wget gpg
@@ -19,7 +21,7 @@ curl -fsSL https://get.jetpack.io/devbox | bash -s -- --force
 
 # Import the Microsoft GPG key
 sudo apt-get install wget gpg
-sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor > packages.microsoft.gpg
+sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee packages.microsoft.gpg > /dev/null
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 sudo rm -f packages.microsoft.gpg
